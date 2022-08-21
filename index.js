72f69571c2ac24d120c30d91a684cef98b47a8ab
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2022-08-21 13:31:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const arrayStringNames = ["Guadalupe", "Ollie", "Aki"]
+function writeCards( arrayStringNames, birthday = "surprise" ) {
+    let thankYouCards = []
+    for ( let i = 0; i < arrayStringNames.length; i++ ) {
+      thankYouCards.push( `Thank you, ${arrayStringNames[i]}, for the wonderful ${birthday} gift!` )
+    }
+    return thankYouCards
+  }
+  writeCards(["Guadalupe", "Ollie", "Aki"], "wedding");
+  
+  function countDown( numberFromTheTop ) {
+    while ( numberFromTheTop > 0 ) {
+      console.log( numberFromTheTop );
+      numberFromTheTop -= 1;
+    }
+    console.log( numberFromTheTop );
+  }
+  numberFromTheTop();
